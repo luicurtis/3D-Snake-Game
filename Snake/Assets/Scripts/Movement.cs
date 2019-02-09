630dbc090733a1d571bb5f2111a524bb8fc97fa1
Kelvin Lo <kello@live.ca>
2019-02-09 11:50:22 -0800

    Added Player Class
    can be moved using wasd and jump.

---
+++ Snake/Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    private float speed = 10f;
+    private float jump = 25f;
+    private float gravity = 30f;
+    private Vector3 Direction = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController control = gameObject.GetComponent<CharacterController>();
+        if (control.isGrounded)
+        {
+            Direction = new Vector3 (Input.GetAxis("Horizontal"), 0, 
+            Input.GetAxis("Vertical"));
+
+            Direction = transform.TransformDirection(Direction);
+            Direction *= speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                Direction.y = jump;
+            }
+        }
+
+        Direction.y -= gravity * Time.deltaTime;
+
+        control.Move(Direction * Time.deltaTime);
+
+    }
+}
