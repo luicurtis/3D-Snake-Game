5f53ca85563bdef2366b4f431c6b6ced9a0a186f
luicutis <lui_curtis@hotmail.com>
2019-02-09 11:49:10 -0800

    Added fixed camera to the player

---
+++ Snake/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 500f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Add forward froce to player
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, -1000 * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, -1000 * Time.deltaTime);
+        }
+    }
+}
