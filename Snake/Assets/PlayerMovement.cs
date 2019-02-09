e73ff4dd80e4398b88cda324fe1568db2cba1515
luicutis <lui_curtis@hotmail.com>
2019-02-09 10:22:18 -0800

    Initial commit

--- Snake/Assets/PlayerMovement.cs
+++ Snake/Assets/PlayerMovement.cs
@@ -2,15 +2,17 @@
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float forwardForce = 2000f; 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, world");
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
     }
 }
