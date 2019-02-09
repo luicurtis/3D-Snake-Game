1b4589ba3bda839505d0bf84c5ee3bc2d0bb7c89
luicutis <lui_curtis@hotmail.com>
2019-02-09 10:07:13 -0800

    Added physics to Player

--- Snake/Assets/PlayerMovement.cs
+++ Snake/Assets/PlayerMovement.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello, world");
     }
 
     // Update is called once per frame
