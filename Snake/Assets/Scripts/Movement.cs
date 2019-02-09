a4ef7bf018001f7afd726876c48f9d8c91a5aaf7
Kelvin Lo <kello@live.ca>
2019-02-09 15:13:29 -0800

    Updated Snake Class with rotation
    rotation and Has body

--- Snake/Assets/Scripts/Movement.cs
+++ Snake/Assets/Scripts/Movement.cs
@@ -2,39 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
-    private float speed = 10f;
-    private float jump = 25f;
-    private float gravity = 30f;
-    private Vector3 Direction = Vector3.zero;
+public class Movement : MonoBehaviour
+{
+
+    public List<Transform> Body = new List<Transform>();
+    public float speed = 1;
+    public float speedRotation = 50;
+    public GameObject Head;
+    public int startSize;
+    public float minimumdist = 3f;
+
+    private float dist;
+    private Transform currentBody;
+    private Transform PreviousBody;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        CharacterController control = gameObject.GetComponent<CharacterController>();
-        if (control.isGrounded)
+        for (int count = 0; count < startSize; count++)
         {
-            Direction = new Vector3 (Input.GetAxis("Horizontal"), 0, 
-            Input.GetAxis("Vertical"));
-
-            Direction = transform.TransformDirection(Direction);
-            Direction *= speed;
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                Direction.y = jump;
-            }
+            addSnakeBody();
         }
 
-        Direction.y -= gravity * Time.deltaTime;
+    }
 
-        control.Move(Direction * Time.deltaTime);
+
+    private void LateUpdate()
+    {
+        SnakeMove();
+        if (Input.GetKey(KeyCode.Q))
+        {
+            addSnakeBody();
+        }
 
     }
+
+    void SnakeMove()
+    {
+
+        float movingspeed = speed;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movingspeed *= 5;
+        }
+
+        Body[0].Translate(Body[0].forward * movingspeed
+            * Time.smoothDeltaTime, Space.World);
+
+        if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0)
+        {
+            Body[0].Rotate(Vector3.up * speedRotation * Time.deltaTime
+                * Input.GetAxis("Horizontal"));
+        }
+
+        for (int counter = 1; counter < Body.Count; counter++)
+        {
+            currentBody = Body[counter];
+            PreviousBody = Body[counter - 1];
+            dist = Vector3.Distance(PreviousBody.position, currentBody.position);
+            Vector3 updateposition = PreviousBody.position;
+            updateposition.y = Body[0].position.y;
+
+            float currentTime = (Time.deltaTime * dist)
+            / (minimumdist * movingspeed);
+
+            if (currentTime > 0.5f)
+            {
+                currentTime = 0.5f;
+            }
+            currentBody.rotation = Quaternion.Slerp(currentBody.rotation, PreviousBody.rotation, currentTime);
+            currentBody.position = Vector3.Slerp(currentBody.position, updateposition, currentTime);
+        }
+    }
+
+
+    void addSnakeBody()
+    {
+        Transform extraPart = (Instantiate(Head, Body[Body.Count - 1].position,
+        Body[Body.Count - 1].rotation) as GameObject).transform;
+        extraPart.SetParent(transform);
+        Body.Add(extraPart);
+    }
+
 }
