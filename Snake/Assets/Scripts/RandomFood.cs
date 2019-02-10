ef4b73cee18d581692540eb9d045cb6c7ae179bb
Kelvin Lo <kello@live.ca>
2019-02-09 17:13:34 -0800

    Updated Random Food Class
    fits within range of board with delay

--- Snake/Assets/Scripts/RandomFood.cs
+++ Snake/Assets/Scripts/RandomFood.cs
@@ -7,22 +7,38 @@ public class RandomFood : MonoBehaviour
 {
     public GameObject Food;
     private Vector3 RandomVector;
+    private float time;
+    private float SpawnInterval = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thread.Sleep(7000);
+        if (time >= SpawnInterval)
+        {
+            this.Spawn();
+            time = 0;
+        }
+        else
+        {
+            time += Time.deltaTime;
+        }
+
+    }
+
+    void Spawn()
+    {
         float x = (Random.Range(-10, 10));
-        float y = (Random.Range(1, 2));
+        float y = 2;
         float z = (Random.Range(-10, 10));
 
-        RandomVector = new Vector3(x, y, z); 
-        Instantiate(Food, RandomVector, Quaternion.identity);
-        
+        RandomVector = new Vector3(x, y, z);
+        Food = Instantiate(Food, RandomVector, Quaternion.identity) as GameObject;
     }
+
 }
